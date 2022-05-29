3ba56c59bafb73313e784ce626cbbbed7b59770c
Ines-a22001936 <inesou02@hotmail.com>
2022-05-29 15:16:43 +0100

    new info

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,4 @@
-import matplotlib
+# import matplotlib
 from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
