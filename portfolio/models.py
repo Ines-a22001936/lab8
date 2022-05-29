59f6bbae099832e4a9be97b73c5d8e15be74d441
Ines-a22001936 <inesou02@hotmail.com>
2022-05-29 19:55:17 +0100

    alterações

--- portfolio/models.py
+++ portfolio/models.py
@@ -49,8 +49,7 @@ class Cadeira(models.Model):
     ano = models.IntegerField()
     topicos = models.TextField()
     etcs = models.IntegerField()
-
-    # docente_teorica = models.ForeignKey(ProfessorTeorica, on_delete=models.CASCADE, default="")
+    docente_teorica = models.ForeignKey(ProfessorTeorica, on_delete=models.CASCADE, default="")
     docente_pratica = models.ForeignKey(ProfessorPratica, on_delete=models.CASCADE, default="")
 
     def __str__(self):
